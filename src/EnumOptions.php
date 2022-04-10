eedbc85f91a734c212fa36f8a80cdcafef4e5fab
Lazizbek Ergashev <lazerg2@gmail.com>
2022-04-10 20:46:49 +0500

    add `selections()` and `selectionsToArray()` methods;

--- src/EnumOptions.php
+++ src/EnumOptions.php
@@ -9,7 +9,7 @@ use UnitEnum;
 trait EnumOptions
 {
     /**
-     * Convert cases of enum to collection options
+     * Convert cases of enum to collection of options
      *
      * @input
      * case RENTAL_TRUCK = 1;
@@ -36,12 +36,46 @@ trait EnumOptions
     }
 
     /**
-     * Convert cases of enum to array options
+     * Convert cases of enum to collection of selections
+     *
+     * @input
+     * case RENTAL_TRUCK = 1;
+     * case CONTAINER = 2;
+     * case FREIGHT_TRAILER = 3;
+     *
+     * @output
+     * [
+     *  0 => ['value' => 1, 'display' => 'Rental Truck'],
+     *  1 => ['value' => 2, 'display' => 'Container'],
+     *  3 => ['value' => 3, 'display' => 'Freight Trailer']
+     * ]
+     *
+     * @return Collection
+     */
+    public static function selections(): Collection
+    {
+        return self::options()
+            ->map(fn($display, $value) => compact('value', 'display'))
+            ->values();
+    }
+
+    /**
+     * Convert cases of enum to array of options
      *
      * @return array
      */
     public static function optionsToArray(): array
     {
         return self::options()->toArray();
+    }
+
+    /**
+     * Convert cases of enum to array of selections
+     *
+     * @return array
+     */
+    public static function selectionsToArray(): array
+    {
+        return self::selections()->toArray();
     }
 }
