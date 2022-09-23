a76387addad97c4c2ee572dc3261ea8a94789b97
lazerg <lazerg2@gmail.com>
2022-09-23 12:36:32 +0500

    wip;

---
+++ tests/EnumTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @noinspection PhpIllegalPsrClassPathInspection
+ *
+ * @method static int VERY_EASY()
+ * @method static int EASY()
+ * @method static int MEDIUM()
+ * @method static int HARD()
+ * @method static int VERY_STRONG()
+ */
+enum LevelTypes: int
+{
+    use \Lazerg\LaravelEnumPro\EnumPro;
+
+    case VERY_EASY = 1;
+    case EASY = 2;
+    case MEDIUM = 3;
+    case STRONG = 4;
+    case VERY_STRONG = 5;
+}
